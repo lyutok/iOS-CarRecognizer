544d64ae8296d3c443f1ec2f2e656845d6a48678
Mila <mila.liutok@gmail.com>
2021-07-20 15:25:24 +0300

    Initial Commit

---
+++ CarRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarRecognizer
+//
+//  Created by Lyudmila Tokar on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
